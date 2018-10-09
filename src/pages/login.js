b578090ae9ca255cafbde8767f828d08f97187b9
Jonghagi <jonghagi77@gmail.com>
2018-10-09 15:27:36 +0900

    Create Login in function without ServerConnect

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,30 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import GoogleLogin from 'react-google-login';
 import './login.css';
-
-// or
-//import { GoogleLogin } from 'react-google-login';
+import { Redirect } from 'react-router-dom';
 
 
-const responseGoogle = (response) => {
-    
-}
+class login extends Component{
+    state = {
+        isLogin: false
+    }
 
-const login = () =>
-{
-    return (
-        <div className="login">
-            <h1 className="title">CYRANO</h1>
-            <GoogleLogin
-                clientId="813143843555-sofm6u14svb2ulfpaun28qubn44tr4ge.apps.googleusercontent.com"
-                buttonText="Google Login"
-                onSuccess={responseGoogle}
-                onFailure={responseGoogle}
-            />
-        </div>
-            //document.getElementById('googleButton');
-    );
+    responseGoogle = (response) => {
+        alert(response.tokenId);
+        this.setState({isLogin: true})
+    }
+
+    render(){
+        return(
+            <div className="login">
+                <h1 className="title">CYRANO</h1>
+                <GoogleLogin
+                    clientId="813143843555-sofm6u14svb2ulfpaun28qubn44tr4ge.apps.googleusercontent.com"
+                    buttonText="Google Login"
+                    onSuccess={this.responseGoogle = this.responseGoogle.bind(this)}
+                    onFailure={this.responseGoogle}
+                />
+
+                {this.state.isLogin ? <Redirect to="/newspeed"/> : ""}
+            </div>
+
+        );
+    }
 }
 
 export default login;
